502e209b3bd1eeec7d7e30dac1ff80d79990963a
pan-jy <2896192354@qq.com>
2024-04-26 14:39:41 +0800

    style: 修改图标

--- src/renderer/src/pages/Home.vue
+++ src/renderer/src/pages/Home.vue
@@ -177,11 +177,11 @@ onUnmounted(() => {
       <i class="pi pi-palette text-xl" />
     </button>
     <button
-      v-tooltip.top="'Call a friend'"
+      v-tooltip.top="'媒体流转发'"
       class="absolute rounded-full w-12 h-12 right-3 top-2"
       @click="avatar.forwardStream"
     >
-      <i class="pi pi-whatsapp text-xl" />
+      <i class="pi pi-send text-xl" />
     </button>
     <svg
       class="w-full h-full"
