7697a99320faed640a44400ab970af3ce2e3758e
pan-jy <2896192354@qq.com>
2024-05-10 18:15:05 +0800

    feat: 上传&删除背景图片

--- src/renderer/src/common/config/Config.ts
+++ src/renderer/src/common/config/Config.ts
@@ -7,7 +7,7 @@ export const configKey = Symbol() as InjectionKey<Config>
 
 export class Config {
   modelList = ref<PresetModelList>([])
-  backgroundImages: BackgroundImage[][] = []
+  backgroundImages = ref<BackgroundImage[][]>([])
 
   constructor() {
     this.initModelList()
@@ -48,14 +48,46 @@ export class Config {
 
   async initBackground() {
     const backgroundList = await window.electron.ipcRenderer.invoke('get-store', 'backgroundList')
-    if (backgroundList) this.backgroundImages = backgroundList
+    if (backgroundList) this.backgroundImages.value = backgroundList
     else {
       window.electron.ipcRenderer.invoke('set-store', 'backgroundList', backgroundImages)
       window.electron.ipcRenderer.invoke('set-store', 'background', {
         type: BackgroundType['2d'],
         value: backgroundImages[0][0].src
       })
-      this.backgroundImages = backgroundImages
+      this.backgroundImages.value = backgroundImages
     }
+  }
+
+  /**
+   *
+   * @param type 0: 2d, 1: 3d
+   * @param {BackgroundImage} background
+   */
+  async uploadBackground(type: number, background: BackgroundImage) {
+    if (type !== 0 && type !== 1) throw new Error()
+    if (this.backgroundImages.value[type].some((item) => item.src === background.src))
+      throw new Error('该背景已存在')
+    this.backgroundImages.value[type].unshift(background)
+    await window.electron.ipcRenderer.invoke(
+      'set-store',
+      'backgroundList',
+      toRaw(this.backgroundImages.value)
+    )
+  }
+
+  async deleteBackground(type: number, background: BackgroundImage) {
+    // this.backgroundImages.value[type] = toRaw(this.backgroundImages.value[type]).filter(
+    // (item) => item.src !== background.src
+    // )
+    this.backgroundImages.value[type].splice(
+      this.backgroundImages.value[type].findIndex((item) => item.src === background.src),
+      1
+    )
+    await window.electron.ipcRenderer.invoke(
+      'set-store',
+      'backgroundList',
+      toRaw(this.backgroundImages.value)
+    )
   }
 }
