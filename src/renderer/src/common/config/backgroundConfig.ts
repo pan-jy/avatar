24d1bb97f61f33a59c997af3c8e3ba0ce40639e1
Pan Junyan <81850790+pan-jy@users.noreply.github.com>
2024-05-11 10:46:08 +0800

    Feat background manage (#10)
    * fix: 修改拼写错误及部分样式
    
    * feat: 上传&删除背景图片

--- src/renderer/src/common/config/backgroundConfig.ts
+++ src/renderer/src/common/config/backgroundConfig.ts
@@ -2,6 +2,7 @@ export interface BackgroundImage {
   name: string
   cover?: string
   src?: string
+  userUpload?: boolean
 }
 
 const resourcesPath = import.meta.glob('@resources/background/**/*.jpg', {
--- src/renderer/src/components/sideBar/customBackground/ImageItem.vue
+++ src/renderer/src/components/sideBar/customBackground/ImageItem.vue
@@ -5,15 +5,21 @@ defineProps<{
   bg: BackgroundImage
   active: boolean
 }>()
+
+defineEmits<{
+  select: [string | undefined]
+  delete: [BackgroundImage]
+}>()
 </script>
 
 <template>
   <div
     v-tooltip.bottom="bg.name"
-    class="rounded-2xl overflow-hidden flex justify-center items-center border-4 border-white outline bg-white/90"
+    class="relative bg-item rounded-2xl overflow-hidden flex justify-center items-center border-4 border-white outline bg-white/90"
     :class="
       active ? 'outline-[7px] outline-primary-300' : 'hover:outline-[5px] hover:outline-white/40'
     "
+    @click="$emit('select', bg.src)"
   >
     <PrImage
       v-if="bg.src"
@@ -26,5 +32,19 @@ defineProps<{
       v-else
       class="w-full h-full bg-[url('/background/2d/transparent.png')] bg-[length:20px]"
     ></div>
+
+    <div
+      v-if="bg.userUpload"
+      class="absolute w-[40px] h-[40px] top-1 right-1 z-10 bg-gray-300 rounded-full items-center justify-center overflow-hidden cursor-pointer outline-white/40 hover:outline hover:outline-4 hidden"
+      @click="$emit('delete', bg)"
+    >
+      <i class="pi pi-minus text-2xl text-red-700" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.bg-item:hover .absolute {
+  display: flex;
+}
+</style>
